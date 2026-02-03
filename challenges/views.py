14a73e0ed02d24a10c2ae0c3118a05c158d7162e
igorsimevski <igor.simevski@vasscompany.com>
2026-02-03 12:22:14 +0100

    added redirects

--- challenges/views.py
+++ challenges/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+
+monthly_challenges = {
+    "january": "Walk for at least 1 minutes every day.",
+    "february": "Walk for at least 2 minutes every day.",
+    "march": "Walk for at least 3 minutes every day.",
+    "april": "Walk for at least 4 minutes every day.",
+    "may": "Walk for at least 5 minutes every day.",
+    "june": "Walk for at least 6 minutes every day.",
+    "july": "Walk for at least 7 minutes every day.",
+    "august": "Walk for at least 8 minutes every day.",
+    "september": "Walk for at least 9 minutes every day.",
+    "october": "Walk for at least 10 minutes every day.",
+    "november": "Walk for at least 11 minutes every day.",
+    "december": "Walk for at least 12 minutes every day.",
+}
+
 
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(f"Challenge for month number: {month}")
+    months = list(monthly_challenges.keys())
+    return HttpResponseRedirect(f"/challenges/{months[month - 1]}")
+
 
 def monthly_challenge(request, month):
-    challenges = {
-        "january": "Walk for at least 1 minutes every day.",
-        "february": "Walk for at least 2 minutes every day.",
-        "march": "Walk for at least 3 minutes every day.",
-        "april": "Walk for at least 4 minutes every day.",
-        "may": "Walk for at least 5 minutes every day.",
-        "june": "Walk for at least 6 minutes every day.",
-        "july": "Walk for at least 7 minutes every day.",
-        "august": "Walk for at least 8 minutes every day.",
-        "september": "Walk for at least 9 minutes every day.",
-        "october": "Walk for at least 10 minutes every day.",
-        "november": "Walk for at least 11 minutes every day.",
-        "december": "Walk for at least 12 minutes every day.",
-    }
 
-    challenge_text = challenges.get(month, "This month is not supported.")
+    challenge_text = monthly_challenges.get(month, "This month is not supported.")
     return HttpResponse(challenge_text)
