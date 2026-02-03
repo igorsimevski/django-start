f34f0d0c28d7fab67b8cd79bb3911f14c5ecedf1
igorsimevski <igor.simevski@vasscompany.com>
2026-02-03 12:18:28 +0100

    added month numbers

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.january, name="january"),
-    path("february", views.february, name="february"),
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(f"Challenge for month number: {month}")
 
-def january(request):
-    return HttpResponse("Hello, world. You're at the challenges index1.")
+def monthly_challenge(request, month):
+    challenges = {
+        "january": "Walk for at least 1 minutes every day.",
+        "february": "Walk for at least 2 minutes every day.",
+        "march": "Walk for at least 3 minutes every day.",
+        "april": "Walk for at least 4 minutes every day.",
+        "may": "Walk for at least 5 minutes every day.",
+        "june": "Walk for at least 6 minutes every day.",
+        "july": "Walk for at least 7 minutes every day.",
+        "august": "Walk for at least 8 minutes every day.",
+        "september": "Walk for at least 9 minutes every day.",
+        "october": "Walk for at least 10 minutes every day.",
+        "november": "Walk for at least 11 minutes every day.",
+        "december": "Walk for at least 12 minutes every day.",
+    }
 
-def february(request):
-    return HttpResponse("Hello, February. Walk 20 minutes every day.")
+    challenge_text = challenges.get(month, "This month is not supported.")
+    return HttpResponse(challenge_text)
