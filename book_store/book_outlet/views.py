71274478b803f666118a30730ab10f7d4e775e2e
igorsimevski <igor.simevski@vasscompany.com>
2026-02-04 12:05:18 +0100

    added book detail

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -5,3 +5,7 @@ from .models import Book
 def index(request):
     books = Book.objects.all()
     return render(request, "book_outlet/index.html", {"books": books})
+
+def book_detail(request, id):
+    book = Book.objects.get(id=id)
+    return render(request, "book_outlet/book_detail.html", {"book": book})
