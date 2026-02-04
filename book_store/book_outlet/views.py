3a92db5e731fadf1bacd5a92271f682be3ff1f34
igorsimevski <igor.simevski@vasscompany.com>
2026-02-04 12:07:49 +0100

    Added 404 handler

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Book
+from django.http import Http404
 
 
 def index(request):
     books = Book.objects.all()
     return render(request, "book_outlet/index.html", {"books": books})
 
+
 def book_detail(request, id):
-    book = Book.objects.get(id=id)
+    try:
+        book = Book.objects.get(id=id)
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist")
     return render(request, "book_outlet/book_detail.html", {"book": book})
