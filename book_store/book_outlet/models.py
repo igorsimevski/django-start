bb2f054c41af73f3f8a6880438cc1ad71328c1fb
igorsimevski <igor.simevski@vasscompany.com>
2026-02-04 13:31:16 +0100

    added many to many in admin

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -8,6 +8,9 @@ class Country(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = "Countries"
 
 class Address(models.Model):
     street = models.CharField(max_length=200)
