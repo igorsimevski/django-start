c6919d4ab461b61379e82d60469fb3686958095c
igorsimevski <igor.simevski@vasscompany.com>
2026-02-04 12:55:18 +0100

    added author foreign key

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(max_length=100, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books", null=True)
     is_bestseller = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
