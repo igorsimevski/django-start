aeba845cf8d57856be7b62775f262aedc8b02062
igorsimevski <igor.simevski@vasscompany.com>
2026-02-04 12:27:10 +0100

    Added total, average

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
 from django.http import Http404
+from django.db.models import Avg
 
 
 def index(request):
     books = Book.objects.all()
-    return render(request, "book_outlet/index.html", {"books": books})
+    number_of_books = books.count()
+    average_rating = books.aggregate(Avg("rating"))["rating__avg"]
+    return render(
+        request,
+        "book_outlet/index.html",
+        {
+            "books": books,
+            "total_books": number_of_books,
+            "average_rating": average_rating,
+        },
+    )
 
 
 def book_detail(request, slug):
