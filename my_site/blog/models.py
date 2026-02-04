c95e1196419e705928b2f15536a913ef21f54f2e
igorsimevski <igor.simevski@vasscompany.com>
2026-02-04 14:21:49 +0100

    added blog db models

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.caption
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    published_date = models.DateField(auto_now=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True, unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, related_name='posts')
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}" 
