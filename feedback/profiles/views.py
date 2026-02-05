ef0e9018565fd2c004572fd1fc968784a75d23ee
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 21:15:56 +0100

    added store_file method

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 
-# Create your views here.
-
+def store_file(file):
+    with open("temp/image.png", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
 
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        print(request.FILES["image"])
+        store_file(request.FILES["image"])
         return HttpResponseRedirect("/profiles")
