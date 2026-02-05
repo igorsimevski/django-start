1800f154fc5a11801d60913bb0cf47846950bce1
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 21:29:46 +0100

    added db model to deal with uploaded files

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from .forms import ProfileForm
+from .models import UserProfile
 
 def store_file(file):
     with open("temp/image.png", "wb+") as dest:
@@ -18,7 +19,9 @@ class CreateProfileView(View):
     def post(self, request):
         submitted_form = ProfileForm(request.POST, request.FILES)
         if submitted_form.is_valid():
-            store_file(request.FILES["image"])
+            # store_file(request.FILES["image"])
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
             return HttpResponseRedirect("/profiles")
         
         return render(request, "profiles/create_profile.html", {
