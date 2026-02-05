8196d9297d2ca1a95a7f859ffd9ea3d7fda5c464
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 12:27:40 +0100

    class based views alternative

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 from .forms import ReviewForm
 from .models import Review
 
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, "reviews/review.html", {"form": form})
+        
+    def post(self, request):
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank_you")
+        return render(request, "reviews/review.html", {"form": form})
+        
 
 def review(request):
     if request.method == "POST":
