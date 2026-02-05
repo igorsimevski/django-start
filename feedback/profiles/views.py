a34fd46d47584f7092dc71499d6f690cb4930964
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 21:35:43 +0100

    upload with CreateView

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
+
 from .forms import ProfileForm
 from .models import UserProfile
 
@@ -9,21 +11,27 @@ def store_file(file):
         for chunk in file.chunks():
             dest.write(chunk)
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form": form
+#         })
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
-        if submitted_form.is_valid():
-            # store_file(request.FILES["image"])
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+#         if submitted_form.is_valid():
+#             # store_file(request.FILES["image"])
+#             profile = UserProfile(image=request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
         
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+#         return render(request, "profiles/create_profile.html", {
+#             "form": submitted_form
+#         })
+
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
