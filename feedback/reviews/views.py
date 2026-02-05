81061ac9fc5f159829e3a7968057b1fa145d4385
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 20:28:07 +0100

    added context to thankyou view

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,21 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 from .forms import ReviewForm
 from .models import Review
+
 
 class ReviewView(View):
     def get(self, request):
         form = ReviewForm()
         return render(request, "reviews/review.html", {"form": form})
-        
+
     def post(self, request):
         form = ReviewForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("/thank_you")
         return render(request, "reviews/review.html", {"form": form})
-        
+
 
 def review(request):
     if request.method == "POST":
@@ -29,7 +31,7 @@ def review(request):
             # )
             # review.save()
             #
-            # Alternatively: 
+            # Alternatively:
             form.save()
             return HttpResponseRedirect("/thank_you")
 
@@ -38,6 +40,11 @@ def review(request):
 
     return render(request, "reviews/review.html", {"form": form})
 
-class ThankYouView(View):
-    def get(self, request):
-        return render(request, "reviews/thank_you.html")
+
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = "This Works"
+        return context
