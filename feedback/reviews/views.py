9010f951f6e2145bbbe04e92b93dce58ae608df1
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 12:03:25 +0100

    writing form data to db

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,20 +1,27 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
+from .models import Review
+
 
 def review(request):
-  if request.method == 'POST':
-    form = ReviewForm(request.POST)
-    if form.is_valid():
-      print(form.cleaned_data)
-      return HttpResponseRedirect("/thank_you")
-    
-  else:
-    form = ReviewForm()
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            review = Review(
+                user_name=data["user_name"],
+                review_text=data["review_text"],
+                rating=data["rating"],
+            )
+            review.save()
+            return HttpResponseRedirect("/thank_you")
 
-  return render(request, "reviews/review.html", {
-    "form": form
-  })
+    else:
+        form = ReviewForm()
+
+    return render(request, "reviews/review.html", {"form": form})
+
 
 def thank_you(request):
-  return render(request, "reviews/thank_you.html")
+    return render(request, "reviews/thank_you.html")
