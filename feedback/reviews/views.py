a4cd84e0ff78e2b76cc00fb53b3c6159f4ef21a9
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 20:42:02 +0100

    introduced ListView template for reviews list

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 from .forms import ReviewForm
 from .models import Review
 
@@ -46,20 +47,17 @@ class ThankYouView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['message'] = "This Works"
+        context["message"] = "This Works"
         return context
 
-class ReviewsListView(TemplateView):
-    template_name ="reviews/review_list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
-    
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+
+
 class SingleReviewView(TemplateView):
-    template_name ="reviews/single_review.html"
+    template_name = "reviews/single_review.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
