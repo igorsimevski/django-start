a73e4a4dd820694b556041db1d3c44e347154597
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 09:50:00 +0100

    updated admin view

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -3,8 +3,8 @@ from .models import Post, Tag, Author
 
 class PostAdmin(admin.ModelAdmin):
   prepopulated_fields = {"slug": ("title",)}
-  list_filter = ("title",)
-  list_display = ("title",)
+  list_filter = ("title", "published_date", "tags")
+  list_display = ("title", "published_date", "author")
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
