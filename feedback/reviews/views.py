fc7876469795538e7ed0af876344c8f6be822b6c
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 11:28:46 +0100

    added django form

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,19 +1,18 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import ReviewForm
 
 def review(request):
   if request.method == 'POST':
-    entered_username = request.POST['username']
-    if entered_username == "":
-      return render(request, "reviews/review.html", {
-        "has_error": True
-      }) 
-    print(entered_username)
-    return HttpResponseRedirect("/thank_you")
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+      print(form.cleaned_data)
+      return HttpResponseRedirect("/thank_you")
 
-  return render(request, "reviews/review.html")
+  form = ReviewForm()
+  return render(request, "reviews/review.html", {
+    "form": form
+  })
 
 def thank_you(request):
-  return render(request, "reviews/review.html", {
-        "has_error": False
-      }) 
+  return render(request, "reviews/thank_you.html")
