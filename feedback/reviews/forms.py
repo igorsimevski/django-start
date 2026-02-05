7d4f1a5643055b4d2272cae4814fa77454d5ca94
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 11:52:40 +0100

    added more fields

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -5,3 +5,5 @@ class ReviewForm(forms.Form):
     "required": "Your name must not be empty",
     "min_length": "Longer please"
   })
+  review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+  rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
