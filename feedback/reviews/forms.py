c82f14ddc7624b081025c0f910738a0fbdcb8fb4
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 12:11:47 +0100

    simplifying form to db mapping

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,9 +1,22 @@
 from django import forms
+from .models import Review
 
-class ReviewForm(forms.Form):
-  user_name = forms.CharField(label="Your name", min_length=2, error_messages={
-    "required": "Your name must not be empty",
-    "min_length": "Longer please"
-  })
-  review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
-  rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
+# class ReviewForm(forms.Form):
+#   user_name = forms.CharField(label="Your name", min_length=2, error_messages={
+#     "required": "Your name must not be empty",
+#     "min_length": "Longer please"
+#   })
+#   review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+#   rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__' # individual: ['user_name', 'review_text', 'rating']
+        # exclude = ['owner_comment']
+        labels = {
+            'user_name': "Your Name", 
+            'review_text': "Comment", 
+            'rating': "Rating"
+        }
