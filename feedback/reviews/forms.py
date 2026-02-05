cc16904273be2e390452f8cf68524ad15313f595
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 11:37:53 +0100

    customizing messages and labels

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-  user_name = forms.CharField()
+  user_name = forms.CharField(label="Your name", min_length=2, error_messages={
+    "required": "Your name must not be empty",
+    "min_length": "Longer please"
+  })
