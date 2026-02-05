1dd1c4f6c45be7a53df3353711f4e3d8cb7d8f2d
igorsimevski <igor.simevski@vasscompany.com>
2026-02-05 11:10:42 +0100

    added redirect on form submit

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,4 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 def review(request):
+  if request.method == 'POST':
+    entered_username = request.POST['username']
+    print(entered_username)
+    return HttpResponseRedirect("/thank_you")
+
   return render(request, "reviews/review.html")
+
+def thank_you(request):
+  return render(request, "reviews/thank_you.html")
