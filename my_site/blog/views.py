8e8c2cc9dbbf4b2a27fd41ccabfa889cda8f3746
igorsimevski <igor.simevski@vasscompany.com>
2026-02-06 10:32:40 +0100

    handling comment form submission

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from datetime import date
 from django.views.generic import ListView, DetailView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .forms import CommentForm
 
 from .models import Post
@@ -100,12 +103,43 @@ class AllPostsView(ListView):
 #         "post": detail,
 #         "post_tags": detail.tags.all()
 #     })
-class SinglePostView(DetailView):
+# class SinglePostView(DetailView):
+#     template_name = 'blog/post-detail.html'
+#     model = Post
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["post_tags"] = self.object.tags.all()
+#         context["comment_form"] = CommentForm()
+#         return context
+
+class SinglePostView(View):
     template_name = 'blog/post-detail.html'
     model = Post
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, 'blog/post-detail.html', context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post_detail", args=[slug]))
+        else:
+            context = {
+                "post": post,
+                "post_tags": post.tags.all(),
+                "comment_form": comment_form
+            }
+            return render(request, 'blog/post-detail.html', context)
+
