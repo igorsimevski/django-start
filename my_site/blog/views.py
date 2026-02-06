38b2770b8c68dc240833ac85a0f202eb2e19f11f
igorsimevski <igor.simevski@vasscompany.com>
2026-02-06 11:38:30 +0100

    added stored posts view

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -153,6 +153,18 @@ class SinglePostView(View):
 
 class ReadLaterView(View):
 
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+        context = {}
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+        return render(request, "blog/stored-posts.html", context)
+
     def post(self, request):
         stored_posts = request.session.get("stored_posts")
         if stored_posts is None:
@@ -162,6 +174,7 @@ class ReadLaterView(View):
 
         if post_id not in stored_posts:
             stored_posts.append(post_id)
+            request.session["stored_posts"] = stored_posts
         
         return HttpResponseRedirect("/")
 
