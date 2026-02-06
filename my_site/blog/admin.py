2c5e8a0e0bae57115dca13e80bcc2cbc3bd531e8
igorsimevski <igor.simevski@vasscompany.com>
2026-02-06 10:57:22 +0100

    added comments in admin

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from .models import Post, Tag, Author
+from .models import Post, Tag, Author, Comment
 
 class PostAdmin(admin.ModelAdmin):
   prepopulated_fields = {"slug": ("title",)}
   list_filter = ("title", "published_date", "tags")
   list_display = ("title", "published_date", "author")
 
+class CommentAdmin(admin.ModelAdmin):
+  list_display = ("user_name", "post")
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
